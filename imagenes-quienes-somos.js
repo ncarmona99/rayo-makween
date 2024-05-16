381115ab4cb70c6d320f4feef8313ca2aa3dab62
visolorza <vi.solorza@duocuc.cl>
2024-05-16 13:27:18 -0400

    Se realiza implementacion de mapa, se cambia archivo de estilo para ajustar el mapa, se crea nuevo archivo js para intentar implementar api

---
+++ imagenes-quienes-somos.js
@@ -0,0 +1,20 @@
+/*const settings = {
+    async: true,
+    crossDomain: true,
+    url: 'https://f1-motorsport-data.p.rapidapi.com/photos?driverId=4665',
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': '42f4a3eb83msh581d80995676a59p1d6a7ajsndb2a90039252',
+        'X-RapidAPI-Host': 'f1-motorsport-data.p.rapidapi.com'
+    }
+};
+
+const driverPhoto = document.getElementById('driverPhoto');
+
+$.ajax(settings).done(function (response) {
+    if (response && response.length > 0) {
+        driverPhoto.src = response[1].imgSrc;
+    } else {
+        driverPhoto.src = 'placeholder.jpg'; 
+    }
+}); */
